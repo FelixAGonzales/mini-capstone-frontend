bc4f5a9c3da7b917f9c3304037b09516d4761025
FelixAGonzales <FelixAGonzales93@gmail.com>
2024-09-26 22:03:28 -0400

    added frontend authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Header } from "./Header";
 import { ProductsPage } from "./ProductsPage";
 import { Footer } from "./Footer";
+import { LoginPage } from "./LoginPage";
+import { SignupPage } from "./SignupPage"
+// import { LogoutLink } from "./LogoutLink";
 
 function App() {
   return (
     <div>
       <Header />
+      {/* <LogoutLink /> */}
+      <SignupPage />
+      <LoginPage />
       <ProductsPage />
       <Footer />
     </div>
